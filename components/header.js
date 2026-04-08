ec3f14c77c8936ea7f489fe53a43cb7cd461f22a
laumart21-bogran <lauramartinezp9@gmail.com>
2026-04-08 11:45:19 -0600

    Create header.js

---
+++ components/header.js
@@ -0,0 +1,23 @@
+document.write(`
+<header class="header">
+  <div class="container">
+    
+    <div class="logo">
+      <img src="logo.png" alt="neXsv logo">
+    </div>
+
+    <nav class="nav">
+      <a href="index.html">Inicio</a>
+      <a href="negocios.html">Marketplace</a>
+      <a href="beneficios.html">Beneficios</a>
+      <a href="blog.html">Blog</a>
+      <a href="como.html">Cómo funciona</a>
+    </nav>
+
+    <div class="cta">
+      <a href="#" class="btn">Ingresar</a>
+    </div>
+
+  </div>
+</header>
+`);
