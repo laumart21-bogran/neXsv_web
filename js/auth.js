fa45049491cacfcdd8c6f6294029e8cc6c54dfc9
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-15 11:51:57 -0600

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,50 @@
+/* ==========================================
+   neXsv Authentication Manager
+========================================== */
 
+const SESSION_KEY = "nexsv.member";
+
+function isLogged() {
+    return localStorage.getItem(SESSION_KEY) !== null;
+}
+
+function getCurrentUser() {
+
+    const user = localStorage.getItem(SESSION_KEY);
+
+    if (!user) return null;
+
+    return JSON.parse(user);
+
+}
+
+function login(userData) {
+
+    localStorage.setItem(
+        SESSION_KEY,
+        JSON.stringify(userData)
+    );
+
+}
+
+function logout() {
+
+    localStorage.removeItem(SESSION_KEY);
+
+}
+
+function requireMember(callback) {
+
+    if (isLogged()) {
+
+        callback();
+
+        return true;
+
+    }
+
+    showMemberModal();
+
+    return false;
+
+}
