756b386951e0044b73d85fc35d0f810bd4420811
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-24 11:16:37 -0600

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+/*=========================================
+neXsv
+Configuración Frontend
+=========================================*/
+
+const API_MEMBERS =
+    "https://script.google.com/macros/s/AKfycbypRvQ3aMPZeT5ciYX-rteUmhIG7ulg42cJdrKy1lSuxZqx6WfroibTm6aifUvbNTH3/exec";
