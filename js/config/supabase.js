efbbdb13d7d9a3ae638805246fdd98a63d3446df
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-27 12:45:32 -0600

    Create supabase.js

---
+++ js/config/supabase.js
@@ -0,0 +1,10 @@
+// ==========================================
+// neXsv | Supabase Client
+// ==========================================
+
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+    "https://verswqljdxiaveicdtfq.supabase.co",
+    "sb_publishable_4jUefYv7Yd6Bw8q31IPTQQ_5eBgILOp"
+);
