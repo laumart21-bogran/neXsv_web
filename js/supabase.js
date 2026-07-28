c86c56fe3250e0f6f6ef1034a61582abcab27326
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-28 08:04:31 -0600

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,67 @@
+/*=========================================
+neXsv
+Supabase Manager
+=========================================*/
 
+const SUPABASE_URL = "https://verswqljdxiaveicdtfq.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_4jUefYv7Yd6Bw8q31IPTQQ_5eBgILOp";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+/*=========================================
+Registrar usuario
+=========================================*/
+async function registerUser(nombre, correo, password){
+
+    const { data, error } = await supabase.auth.signUp({
+
+        email: correo,
+        password: password
+
+    });
+
+    if(error){
+
+        return{
+
+            success:false,
+            message:error.message
+
+        };
+
+    }
+
+    const { error: profileError } =
+    await supabase
+    .from("profiles")
+    .insert({
+
+        auth_user_id: data.user.id,
+        nombre: nombre,
+        email: correo
+
+    });
+
+    if(profileError){
+
+        return{
+
+            success:false,
+            message:profileError.message
+
+        };
+
+    }
+
+    return{
+
+        success:true,
+        user:data.user
+
+    };
+
+}
