8fb55da0ca290f598740282e6f0a0a204695d579
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-30 10:02:14 -0600

    Update supabase-client.js

--- js/core/supabase-client.js
+++ js/core/supabase-client.js
@@ -1,1 +1,8 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
+import { APP_CONFIG } from "./config.js";
+
+export const supabase = createClient(
+    APP_CONFIG.supabaseUrl,
+    APP_CONFIG.supabaseAnonKey
+);
