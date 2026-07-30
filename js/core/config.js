cb76ac7d35746e15eb0f5fb52d8100cfbb8ab832
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-30 10:01:52 -0600

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,10 @@
+export const APP_CONFIG = {
+    supabaseUrl: "https://TU-PROYECTO.supabase.co",
+    supabaseAnonKey: "TU_ANON_KEY",
 
+    routes: {
+        login: "/acceso/login-usuario.html",
+        dashboard: "/dashboard.html",
+        unauthorized: "/403.html"
+    }
+};
