fbe0bd96e6734949608f4941851ca53351057782
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-30 10:19:06 -0600

    Update supabase-client.js

--- js/core/supabase-client.js
+++ js/core/supabase-client.js
@@ -1,8 +1,17 @@
+/**
+ * ==========================================================
+ * Cliente único de Supabase
+ * ==========================================================
+ */
+
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 import { APP_CONFIG } from "./config.js";
 
 export const supabase = createClient(
-    APP_CONFIG.supabaseUrl,
-    APP_CONFIG.supabaseAnonKey
+
+    APP_CONFIG.supabase.url,
+
+    APP_CONFIG.supabase.anonKey
+
 );
