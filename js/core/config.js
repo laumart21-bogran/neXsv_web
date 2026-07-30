d19e5c3073e7033878f49f5188bcbde95be0ef44
laumart21-bogran <lauramartinezp9@gmail.com>
2026-07-30 10:18:02 -0600

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,10 +1,30 @@
+/**
+ * ==========================================================
+ * neXsv Platform v2
+ * Configuración global de la aplicación
+ * ==========================================================
+ */
+
 export const APP_CONFIG = {
-    supabaseUrl: "https://TU-PROYECTO.supabase.co",
-    supabaseAnonKey: "TU_ANON_KEY",
+
+    appName: "neXsv",
+
+    environment: "development",
+
+    supabase: {
+
+        url: "https://verswqljdxiaveicdtfq.supabase.co",
+
+        anonKey: "sb_publishable_4jUefYv7Yd6Bw8q31IPTQQ_5eBgILOp"
+
+    },
 
     routes: {
+
         login: "/acceso/login-usuario.html",
-        dashboard: "/dashboard.html",
-        unauthorized: "/403.html"
+
+        dashboard: "/dashboard.html"
+
     }
+
 };
