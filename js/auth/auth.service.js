58429c08f0c67b793cfd1a711638f6c35d77a301
laumart21-bogran <lauramartinezp9@gmail.com>
2026-08-11 09:47:43 -0600

    Update auth.service.js

--- js/auth/auth.service.js
+++ js/auth/auth.service.js
@@ -49,14 +49,20 @@ class AuthService {
 
     }
 
-    /**
-     * Recuperar contraseña
-     */
-    async resetPassword(email) {
+/**
+ * Recuperar contraseña
+ */
+async resetPassword(email) {
 
-        return await supabase.auth.resetPasswordForEmail(email);
+    return await supabase.auth.resetPasswordForEmail(email, {
 
-    }
+        redirectTo:
+            window.location.origin +
+            "/acceso/nueva-password.html"
+
+    });
+
+}
 
     /**
      * Obtener sesión actual
