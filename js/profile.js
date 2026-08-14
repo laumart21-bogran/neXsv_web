02f1cce79e23b0c46bff4711f7536c59c55502ce
laumart21-bogran <lauramartinezp9@gmail.com>
2026-08-14 10:36:14 -0600

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -5,8 +5,15 @@ console.log("Profile.js cargado correctamente");
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-    const { data: { user } } = await supabase.auth.getUser();
+    const {
+        data: { user }
+    } = await supabase.auth.getUser();
 
     console.log("Usuario:", user);
+
+    const { data, error } = await ProfileService.getProfile(user.id);
+
+    console.log("Perfil:", data);
+    console.log("Error:", error);
 
 });
