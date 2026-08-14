83743b2f305aa604463d3ca14fe0119e39668501
laumart21-bogran <lauramartinezp9@gmail.com>
2026-08-14 10:32:45 -0600

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,3 +1,12 @@
+import { supabase } from "./core/supabase-client.js";
 import ProfileService from "./services/profile.service.js";
 
 console.log("Profile.js cargado correctamente");
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const { data: { user } } = await supabase.auth.getUser();
+
+    console.log("Usuario:", user);
+
+});
