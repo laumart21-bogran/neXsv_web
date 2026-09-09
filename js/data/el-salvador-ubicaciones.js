c5c35c95a4daa5e6a67a9e65abd195b2cbf29e6b
laumart21-bogran <lauramartinezp9@gmail.com>
2026-09-09 11:11:53 -0600

    Agregar catálogo oficial de departamentos y municipios de El Salvador

---
+++ js/data/el-salvador-ubicaciones.js
@@ -0,0 +1,76 @@
+export const UBICACIONES_EL_SALVADOR = {
+    "Ahuachapán": [
+        "Ahuachapán Norte",
+        "Ahuachapán Centro",
+        "Ahuachapán Sur"
+    ],
+    "Santa Ana": [
+        "Santa Ana Norte",
+        "Santa Ana Centro",
+        "Santa Ana Este",
+        "Santa Ana Oeste"
+    ],
+    "Sonsonate": [
+        "Sonsonate Norte",
+        "Sonsonate Centro",
+        "Sonsonate Este",
+        "Sonsonate Oeste"
+    ],
+    "La Libertad": [
+        "La Libertad Norte",
+        "La Libertad Centro",
+        "La Libertad Oeste",
+        "La Libertad Este",
+        "La Libertad Costa",
+        "La Libertad Sur"
+    ],
+    "Chalatenango": [
+        "Chalatenango Norte",
+        "Chalatenango Centro",
+        "Chalatenango Sur"
+    ],
+    "Cuscatlán": [
+        "Cuscatlán Norte",
+        "Cuscatlán Sur"
+    ],
+    "Cabañas": [
+        "Cabañas Este",
+        "Cabañas Oeste"
+    ],
+    "La Paz": [
+        "La Paz Oeste",
+        "La Paz Centro",
+        "La Paz Este"
+    ],
+    "San Salvador": [
+        "San Salvador Norte",
+        "San Salvador Oeste",
+        "San Salvador Este",
+        "San Salvador Centro",
+        "San Salvador Sur"
+    ],
+    "La Unión": [
+        "La Unión Norte",
+        "La Unión Sur"
+    ],
+    "Usulután": [
+        "Usulután Norte",
+        "Usulután Este",
+        "Usulután Oeste"
+    ],
+    "San Vicente": [
+        "San Vicente Norte",
+        "San Vicente Sur"
+    ],
+    "San Miguel": [
+        "San Miguel Norte",
+        "San Miguel Centro",
+        "San Miguel Oeste"
+    ],
+    "Morazán": [
+        "Morazán Norte",
+        "Morazán Sur"
+    ]
+};
+
+export const DEPARTAMENTOS_EL_SALVADOR = Object.keys(UBICACIONES_EL_SALVADOR);
